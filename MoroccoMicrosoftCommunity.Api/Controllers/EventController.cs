0bace4662247cee3ae915e9f4cc4ce66beb85497
AMAlbehg <benmoussa17amal@gmail.com>
2024-02-04 23:20:37 +0100

    add  event CRUD

--- MoroccoMicrosoftCommunity.Api/Controllers/EventController.cs
+++ MoroccoMicrosoftCommunity.Api/Controllers/EventController.cs
@@ -13,17 +13,17 @@ namespace MoroccoMicrosoftCommunity.Api.Controllers
     {
         private readonly IEventRepo _eventRepo;
         private readonly IMapper _mapper;
-        public EventController(IEventRepo eventRepo , IMapper mapper)
+        public EventController(IEventRepo eventRepo, IMapper mapper)
         {
-            _eventRepo = eventRepo; 
+            _eventRepo = eventRepo;
             _mapper = mapper;
-            
+
         }
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Evenement>))]
-        public async Task <IActionResult> GetEvent()
+        public async Task<IActionResult> GetEvent()
         {
-            var evenements = _mapper.Map<List<EventDto>>( await _eventRepo.GetAllAsync());
+            var evenements = _mapper.Map<List<EventDto>>(await _eventRepo.GetAllAsync());
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
             return Ok(evenements);
@@ -31,15 +31,69 @@ namespace MoroccoMicrosoftCommunity.Api.Controllers
         [HttpGet("{eventId}")]
         [ProducesResponseType(200, Type = typeof(Evenement))]
         [ProducesResponseType(400)]
-        public async Task <IActionResult> GetEventById(int eventId)
+        public async Task<IActionResult> GetEventById(int eventId)
         {
             //if (!_eventRepo.IsExists(eventId))
-              //  return NotFound();
+            //  return NotFound();
             var evenement = _mapper.Map<Evenement>(await _eventRepo.GetById(eventId));
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
             return Ok(evenement);
 
         }
+        [HttpPost, DisableRequestSizeLimit]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> CreateEvent([FromBody] EventDto eventDto)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (eventDto == null)
+                return BadRequest("Invalid data");
+
+            var eventEntity = _mapper.Map<Evenement>(eventDto);
+            var resulta = await _eventRepo.Add(eventEntity);
+            return Ok(resulta);
+        }
+        [HttpPut("{eventId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> UpdateEvent(int eventId, [FromBody] EventDto eventDto)
+        {
+            try
+            {
+                if (eventDto == null || eventId != eventDto.EvenementId)
+                {
+                    return BadRequest();
+                }
+
+                var existingEvent = await _eventRepo.GetById(eventId);
+
+                if (existingEvent == null)
+                {
+                    return NotFound();
+                }
+
+                // Mettez à jour les propriétés de l'événement existant avec celles du DTO
+                _mapper.Map(eventDto, existingEvent);
+
+                await _eventRepo.Update(existingEvent);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                // Loggez l'exception ou renvoyez une réponse d'erreur appropriée
+                return StatusCode(500, $"Une erreur s'est produite : {ex.Message}");
+            }
+        }
+
+
+       
+
+
     }
 }
